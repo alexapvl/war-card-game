f7f0075afc4cf58f6f9bb7a9bf50ed6f00eacf35
alexapvl <alex.robite@gmail.com>
2024-07-03 12:21:42 +0300

    commit

--- War-Card-Game/ContentView.swift
+++ War-Card-Game/ContentView.swift
@@ -70,11 +70,11 @@ struct ContentView: View {
     
     func deal() {
         // Randomize the players card
-        var playerCardValue = Int.random(in: 2...14)
+        let playerCardValue = Int.random(in: 2...14)
         playerCard = "card" + String(playerCardValue)
         
         // Randomize the cpus card
-        var cpuCardValue = Int.random(in: 2...14)
+        let cpuCardValue = Int.random(in: 2...14)
         cpuCard = "card" + String(cpuCardValue)
         
         // Update the scores
